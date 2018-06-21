edb30b6905e14e6cdf807ccbf84e3549fd37d68f
root <root@centos7.se2.ipsoft.com>
2018-06-21 12:25:17 +0200

    updates

--- ifsExample.sh
+++ ifsExample.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 LINE=`cat /etc/passwd | grep $1`
-set $LINE
- 
+IFS=':'
+set -- $LINE
+echo "User Name = $1"  
+echo "Password = $2"  
+echo "UID = $3"  
+echo "GID = $4"  
+echo "Description = $5"  
+echo "Home Directory = $6"  
+echo "Current Shell = $7"  
 
 
 

60edf078b6102e81c328e515d3da86f39f2dd414
Shehbab-Kakkar <shehbab1@gmail.com>
2018-06-25 09:08:02 +0530

    Update creatingFileFromShell.sh

--- creatingFileFromShell.sh
+++ creatingFileFromShell.sh
@@ -1,1 +1,1 @@
-FILENAME=difference-between-del-remove-and-pop-on-lists ; touch ${FILENAME}-sample.py ; chmod +x ${FILENAME}-sample.py
+FILENAME=file ; touch ${FILENAME}-sample.py ; chmod +x ${FILENAME}-sample.py
